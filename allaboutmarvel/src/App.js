74fa6e99b1e4e59b6a04397a8af2174f53a79e80
ram-ha <dlgkfka2616@gmail.com>
2023-09-11 04:36:39 +0900

    marvel comics

--- allaboutmarvel/src/App.js
+++ allaboutmarvel/src/App.js
@@ -1,12 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
-import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <Sidebar /> */}
       <Routes>
         <Route path="/character/:id" element={<Detail />} />
         <Route path="/" element={<Home />} />
--- allaboutmarvel/src/routes/Detail.js
+++ allaboutmarvel/src/routes/Detail.js
@@ -19,32 +19,31 @@ function Detail() {
     }, [])
     return (
         <>
-            <div className='flex flex-start h-full'>
-                <div className="flex flex-col items-center w-1/4 p-3 bg-stone-900">
-                    <div className="flex flex-col items-center">
-                        <img className="w-1/2 pt-6" src={'/marvel_logo.png'} alt="logo" />
+            {loading ? (
+                <strong className="absolute inset-y-2/4 inset-x-2/4">Please Wait...<br />Loading...</strong>
+            ) : (
+                <div className='flex flex-start h-full'>
+                    <div className="flex flex-col items-center w-1/4 p-3 bg-stone-900">
+                        <div className="flex flex-col items-center">
+                            <img className="w-1/2 pt-6" src={'/marvel_logo.png'} alt="logo" />
+                            {detail.map((detail) => (
+                                <h1 className="font-serif text-center text-lg text-red-500" key={detail.id}>{detail.name}</h1>
+                            ))}
+                        </div>
+                    </div>
+                    <div className='flex flex-col w-full p-5 justify-between'>
                         {detail.map((detail) => (
-                            <h1 className="font-serif text-center text-lg text-red-500" key={detail.id}>{detail.name}</h1>
+                            <DetailItem
+                                key={detail.id}
+                                id={detail.id}
+                                comics={detail.comics.items}
+                                series={detail.series.items}
+                                stories={detail.stories.items}
+                            />
                         ))}
                     </div>
                 </div>
-                <div className='flex flex-col w-full p-5 justify-between'>
-                    {detail.map((detail) => (
-                        <DetailItem
-                            key={detail.id}
-                            id={detail.id}
-                            comics={detail.comics.items}
-                            series={detail.series.items}
-                            stories={detail.stories.items}
-                        />
-                    ))}
-                </div>
-            </div>
-
-
-            <ul>
-
-            </ul>
+            )}
         </>
     )
 }
